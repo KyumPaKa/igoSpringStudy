bc1b7b439598599f010bcff5fe6e5c705f2e3820
kyumincho <wras456@naver.com>
2019-11-20 18:03:12 +0900

    191120

---
+++ spring03_boot/src/main/java/com/example/spring03/Spring03BootApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 스프링 부트 애플리케이션 bean(시작 클래스)
+@SpringBootApplication
+public class Spring03BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03BootApplication.class, args);
+	}
+
+}
